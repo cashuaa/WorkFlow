afbf671217419ad2a439759ce7172d6464a504cd
jmontesoca <jmontesdeoca@nevada.unr.edu>
2020-04-24 00:08:38 -0700

    stats processed and report data collected and displayed.  Founder Testing in the list of founders is the processing data correctly, since other forms are not matched to founders in the list at the moment

---
+++ flutterApp/lib/startupdata.dart
@@ -0,0 +1,11 @@
+class StartUpData
+{
+  String firstName;
+  String lastName;
+  String email;
+  String startUpName;
+  double weight;
+  List<int> vals;
+
+  StartUpData({this.firstName, this.lastName, this.email, this.startUpName, this.vals, this.weight = 0});
+}
